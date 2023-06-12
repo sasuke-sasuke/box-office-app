b2a7e37972a4135da24c633f808b0f1fd361e26e
Jeof Petty <jeofpetty@icloud.com>
2023-06-12 01:40:21 -0400

    installed react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
-  return <div>konichiwa</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/starred">Go to starred</Link>
+    </div>
+  );
+};
+
+export default Home;
